2c7c36b1c088b4da8f3acc2ba64e035898adb476
otubi <amosoj10@gmail.com>
2023-10-31 13:31:47 +0300

    POSH WEBSITE

--- data/views.py
+++ data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    welcome_message = "Welcome to our awesome Django app!"
+    welcome_message = "Welcome to our website! \n We are Here to help you with your all designs "
     return render(request, 'myapp/home.html', {'welcome_message': welcome_message})
